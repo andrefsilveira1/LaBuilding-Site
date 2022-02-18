4805fffc2e76751585568464b6fd329bf9402fa5
jorgerichard001 <jorgericharduf@gmail.com>
2022-02-18 15:04:32 -0300

    Adicionando pagina de formulario, componentes incluindo o button e parte hero da pagina inicial

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import About from './About/About';
 import Abstract from './Abstract/Abstract';
 import Contact from './Contact/Contact';
+import Home1 from './Home-hero/Home1'
 
 export default function Home(){
     //Aqui deve ser colocado todos os componentes da página Home.
     return(
         <>
+            <Home1/>
             <About/>
             <Abstract/>
             <Contact/>
